644979da9856fcf2dc6f5a66b594d46ecd8fe02d
Matthew Witman <mnwranger@hotmail.com>
2016-08-09 00:42:51 -0400

    Working on a decent front facing web app. Currently using my Stylus task from Dissipate

---
+++ src/main/kotlin/com/github/sirlag/Handlers/RouteHandlers.kt
@@ -0,0 +1,90 @@
+package com.github.sirlag.Handlers
+
+import com.github.sirlag.Dependency
+import com.github.sirlag.DependencyVersion
+import com.github.sirlag.database
+import com.github.sirlag.db.DependencyAlreadyExistsException
+import com.github.sirlag.db.DependencyNotFoundException
+import com.github.sirlag.db.VersionAlreadyExistsExceptions
+import com.github.sirlag.gson
+import com.google.gson.JsonParseException
+import spark.ModelAndView
+import spark.Request
+import spark.Response
+import spark.Spark
+import java.util.*
+
+fun HandleDependencyRequest(request: Request, response: Response): String {
+    val identifier = request.params(":identifier")
+    val version = request.params(":version")
+
+    var responseMessage = ""
+
+    try {
+        if (version == "latest")
+            responseMessage = gson.toJson(database.getLatestDependencyVersion(identifier))
+        else
+            responseMessage = gson.toJson(database.getDependencyVersion(identifier, version.toDouble()))
+    } catch (ex: DependencyNotFoundException) {
+        //TODO log the exception
+        Spark.halt(404)
+    }
+
+    return responseMessage
+}
+
+fun HandleNewDependencyVersion(request: Request, response: Response): String{
+    try {
+        val version = gson.fromJson(request.body(), DependencyVersion::class.java)
+        database.addVersionToDependency(request.params(":identifier"), version)
+        return version.toString()
+    } catch (ex: JsonParseException){
+        response.status(400)
+        return "Malformed JSON Request"
+    } catch (ex: VersionAlreadyExistsExceptions){
+        response.status(500)
+        return "Version already exists in repository"
+    }
+}
+
+fun HandleNewDependency(request: Request, response: Response): String{
+    try {
+        val dependency = gson.fromJson(request.body(), Dependency::class.java)
+        database.addDependency(dependency)
+        return dependency.toString()
+    } catch (ex: JsonParseException){
+        response.status(400)
+        return "Malformed JSON Request"
+    } catch (ex: DependencyAlreadyExistsException){
+        response.status(500)
+        return "Dependency already exists in repository"
+    }
+}
+
+fun HandleDependencySearch(request: Request, response: Response): ModelAndView {
+    val model = HashMap<String, Any>()
+    val query = request.queryParams("query")
+    val queryResults = database.searchForDependencies(query)
+    if (queryResults.size == 1)
+        response.redirect("/package/${queryResults.first().identifier}")
+    model.put("dependencies", queryResults)
+    return ModelAndView(model, "SearchResults.jade")
+}
+
+fun HandleDependencyPage(request: Request, response: Response): ModelAndView {
+    val model = HashMap<String, Any>()
+    val dependency = database.getDependency(request.params("identifier"))
+    if (dependency == null){
+        response.redirect("/errorPage")
+        response.status(404)
+    } else {
+        model.put("dependency", dependency)
+        model.put("latestVersion", dependency.versions.sortedByDescending { it.version }.first())
+    }
+    return ModelAndView(model, "PackagePage.jade")
+}
+
+fun HandleIndex(request: Request, response: Response): ModelAndView{
+    val model = HashMap<String, Any>()
+    return ModelAndView(model, "Index.jade")
+}
--- src/main/kotlin/com/github/sirlag/Main.kt
+++ src/main/kotlin/com/github/sirlag/Main.kt
@@ -1,22 +1,24 @@
 package com.github.sirlag
 
 import com.github.sirlag.db.*
+import com.github.sirlag.Handlers.*
 import com.google.gson.Gson
-import com.google.gson.JsonParseException
-import spark.ModelAndView
-import spark.Request
-import spark.Response
 import spark.Spark.*
 import spark.template.jade.JadeTemplateEngine
-import java.util.*
+import spark.debug.DebugScreen.enableDebugScreen
 
 val gson = Gson()
 
 val database: RepositoryDatabase = MongoWrapper("192.168.99.100", "test", gson)
 
 fun main(args: Array<String>) {
+    enableDebugScreen()
 
-    get("/search", {request, response -> HandleDependencySearch(request, response)}, JadeTemplateEngine())
+    staticFiles.location("/public")
+
+    get("/", {request, response -> HandleIndex(request, response)}, JadeTemplateEngine())
+    get("search", {request, response -> HandleDependencySearch(request, response)}, JadeTemplateEngine())
+    get("package/:identifier", {request, response -> HandleDependencyPage(request, response)}, JadeTemplateEngine())
 
     //Repo end points.
     get("repo/:identifier") { request, response -> response.redirect("repo/${request.params("identifier")}/latest")}
@@ -26,63 +28,6 @@ fun main(args: Array<String>) {
     post("repo/:identifier/:version") {request, response -> HandleNewDependencyVersion(request, response)}
 
     after { request, response -> response.header("Content-Encoding", "gzip") }
-}
-
-fun HandleDependencyRequest(request: Request, response: Response): String {
-    val identifier = request.params(":identifier")
-    val version = request.params(":version")
-
-    var responseMessage = ""
-
-    try {
-        if (version == "latest")
-            responseMessage = gson.toJson(database.getLatestDependencyVersion(identifier))
-        else
-            responseMessage = gson.toJson(database.getDependencyVersion(identifier, version.toDouble()))
-    } catch (ex: DependencyNotFoundException) {
-        //TODO log the exception
-        halt(404)
-    }
-
-    return responseMessage
-}
-
-fun HandleNewDependencyVersion(request: Request, response: Response): String{
-    try {
-        val version = gson.fromJson(request.body(), DependencyVersion::class.java)
-        database.addVersionToDependency(request.params(":identifier"), version)
-        return version.toString()
-    } catch (ex: JsonParseException){
-        response.status(400)
-        return "Malformed JSON Request"
-    } catch (ex: VersionAlreadyExistsExceptions){
-        response.status(500)
-        return "Version already exists in repository"
-    }
-}
-
-fun HandleNewDependency(request: Request, response: Response): String{
-    try {
-        val dependency = gson.fromJson(request.body(), Dependency::class.java)
-        database.addDependency(dependency)
-        return dependency.toString()
-    } catch (ex: JsonParseException){
-        response.status(400)
-        return "Malformed JSON Request"
-    } catch (ex: DependencyAlreadyExistsException){
-        response.status(500)
-        return "Dependency already exists in repository"
-    }
-}
-
-fun HandleDependencySearch(request: Request, response: Response): ModelAndView{
-    val model = HashMap<String, Any>()
-    val query = request.queryParams("query")
-    val queryResults = database.searchForDependencies(query)
-    if (queryResults.size == 1)
-        response.redirect("/package/${queryResults.first().identifier}")
-    model.put("dependencies", queryResults)
-    return ModelAndView(model, "SearchResults.jade")
 }
 
 data class DependencyVersion(val version: Double, val dependencies: Array<String>, val repositories: Array<String>)
